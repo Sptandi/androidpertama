0d1a62ee8644b91a62c0fefe2fb562236121bbc6
Sptandi <sptandi@gmail.com>
2016-12-03 16:41:36 +0700

    commit 2

--- app/src/main/java/com/example/android/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/android/helloworld/HomeActivity.java
@@ -9,5 +9,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // TODO: 03/12/2016  Something here.... 
+        // TODO: 03/12/2016 Coboooo......  
     }
 }
